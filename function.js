fbac85af836f4a4a5a36b3e34b424c3d2648eab0
Imon Hasan <imon.hasan.dev@gmail.com>
2022-10-13 09:21:34 +0600

    birthday wish

---
+++ function.js
@@ -0,0 +1,15 @@
+// const body = document.querySelector('body');
+
+// body.addEventListener('click',function(){
+//     let audio = document.querySelector('#HBD');
+//     audio.play();
+// })
+
+setTimeout(() => {
+    alert('TOUCH THE CAKE 🎂 AND ENJOY THE PARTY 🎶🎉🎊');
+}, 1000);
+
+window.addEventListener('click', function() {
+    var audio = document.getElementById('HBD');
+    audio.play();
+});
